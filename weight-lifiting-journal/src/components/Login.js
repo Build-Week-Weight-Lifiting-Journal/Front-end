59e90d09e15ff959f245560aa7fe0bb831eb8ea8
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-18 16:31:57 -0600

    general form with some styles

--- weight-lifiting-journal/src/components/Login.js
+++ weight-lifiting-journal/src/components/Login.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components"
 
-const Login = () => {
-    return(
-        <div>
-            <h1>Sign In</h1>
-        </div>
-    )
-}
 
+const ContainerDiv = styled.div`
+        display:flex;
+        flex-direction:column;
+        justify-content:space-evenly;
+        width:35%;
+        
+        margin: 0 auto;
+
+      
+
+            label{
+                justify-content:space-evenly;
+                padding:25px 0;
+            }
+            button{
+                width:15%;
+                margin: 1% auto;
+                
+            }
+
+
+`
+
+const Login = props => {
+    const [login, setLogin] = useState({ userName: "", password: "" });
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      setLogin({ userName: "", password: "" });
+    };
+  
+    const handleChange = event => {
+      setLogin({ ...login, [event.target.name]: event.target.value });
+      console.log(event.target.name, event.target.value);
+    };
+
+    return (
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+        <ContainerDiv>
+
+          <label>
+            UserName:
+            <input
+              required
+              type="text"
+              name="userName"
+              placeholder="Username"
+              value={login.userName}
+              onChange={handleChange}
+              />
+          </label>
+  
+          <label>
+            Password:
+            <input
+              required
+              type="password"
+              name="password"
+              placeholder="password"
+              value={login.password}
+              onChange={handleChange}
+              />
+          </label>
+  
+          <button>Log In</button>
+          <button>Clear</button>
+
+        </ContainerDiv>
+        </form>
+      </div>
+    );
+  };
+  
 export default Login;
