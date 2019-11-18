a95f5cf7fce1048a36c7f5526e42c4aeaded7e91
Krunal Patel <Pkrunal616@gmail.com>
2019-11-17 19:54:54 -0500

    setting up things

---
+++ weight-lifiting-journal/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './components/Home';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+
+        <Route exact path='/' component={Home} />
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
