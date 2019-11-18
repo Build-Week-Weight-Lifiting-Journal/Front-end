312bf52fb6855e68c5f1a96221d1cef0060baaf7
Krunal Patel <Pkrunal616@gmail.com>
2019-11-18 00:05:05 -0500

    setting up things

--- weight-lifiting-journal/src/App.js
+++ weight-lifiting-journal/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home';
+import Nav from './components/Nav';
 
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav />
 
         <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/sign-up' component={SignUp} />
 
       </div>
     </Router>
