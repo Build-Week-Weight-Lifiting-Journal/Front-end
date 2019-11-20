b720d2427dc69141c2495ea35974982320b444ac
krunal Patel <pkrunal616@gmail.com>
2019-11-20 18:14:24 -0500

    user can make workout + add + delete + update

--- weight-lifiting-journal/src/App.js
+++ weight-lifiting-journal/src/App.js
@@ -7,12 +7,13 @@ import Nav from "./components/Nav";
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-// import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute';
 import WorkoutList from './components/WorkoutList';
 import Workout from "./components/Workout";
 import CreateWorkoutForm from './components/CreateWorkoutForm';
 import axiosWithAuth from "./utils/axiosWithAuth";
-import { get } from "http";
+import EditWorkoutForm from './components/EditWorkout';
+
 
 function App() {
   const [workouts, setWorkouts] = useState([]);
@@ -43,9 +44,10 @@ function App() {
         <Route exact path='/sign-up' component={SignUp} />
 
         {/* <PrivateRoute path='/workout' component={WorkoutList} /> */}
-        <Route  path='/workout-list' component={WorkoutList} />
-        <Route  path='/add-workout' component={CreateWorkoutForm}/>
+        <PrivateRoute  path='/workout-list' component={WorkoutList} />
+        <PrivateRoute  path='/add-workout' component={CreateWorkoutForm}/>
         <Route exact path="/workout/:id" render={props => <Workout {...props} workouts={workouts} />} />
+        <PrivateRoute  path='/edit-workout/:id' component={EditWorkoutForm} />
 
       </div>
     </Router>
--- weight-lifiting-journal/src/components/Home.js
+++ weight-lifiting-journal/src/components/Home.js
@@ -1,24 +1,7 @@
-import React,{useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
 
 
 const Home = () => {
-
-    // useEffect(()=> {
-    //     axios
-    //     .get(`https://weight-lift-journal-dev.herokuapp.com/api/users`)
-    //     .then(res => console.log(res.data))
-    //     .catch(err => err);
-
-    // },[])
-
-    // useEffect(()=> {
-    //     axios
-    //     .get(`https://weight-lift-journal-dev.herokuapp.com/api/users/getby/6`)
-    //     .then(res => console.log(res.data))
-    //     .catch(err => err);
-
-    // },[])
 
     return(
         <div>
