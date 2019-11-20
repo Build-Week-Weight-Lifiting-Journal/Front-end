35c2df53ccf0cd06ffd1e46da3dc3f0b5df6495e
danhamraj <danhamraj@gmail.com>
2019-11-20 12:49:26 -0500

    git request for workout card

--- weight-lifiting-journal/src/action/index.js
+++ weight-lifiting-journal/src/action/index.js
@@ -1,56 +1,72 @@
-import axiosWithAuth  from '../utils/axiosWithAuth';
+import axiosWithAuth from "../utils/axiosWithAuth";
 import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
 
-export const USER_LOGING_IN = 'USER_LOGING_IN';
-export const USER_LOGING_IN_SUCCESS = 'USER_LOGING_IN_SUCCESS';
-export const USER_LOGING_IN_FAILURE = 'USER_LOGING_IN_FAILURE';
+export const USER_LOGING_IN = "USER_LOGING_IN";
+export const USER_LOGING_IN_SUCCESS = "USER_LOGING_IN_SUCCESS";
+export const USER_LOGING_IN_FAILURE = "USER_LOGING_IN_FAILURE";
 
-export const login = (loginData) => dispatch => {
-    dispatch( { type: USER_LOGING_IN } )
+export const login = loginData => dispatch => {
+  dispatch({ type: USER_LOGING_IN });
 
-    axiosWithAuth()
+  axiosWithAuth()
     .post(`/api/auth/login`, loginData)
-    .then(response =>  dispatch({ type: USER_LOGING_IN_SUCCESS, payload: response.data.user },
-        localStorage.setItem('token', response.data.token),
-        history.push('/workout')
-    ))
-    .catch(err => dispatch({ type: USER_LOGING_IN_FAILURE, payload: err.response }))
-}
+    .then(response =>
+      dispatch(
+        { type: USER_LOGING_IN_SUCCESS, payload: response.data.user },
+        localStorage.setItem("token", response.data.token),
+        history.push("/workout")
+      )
+    )
+    .catch(err =>
+      dispatch({ type: USER_LOGING_IN_FAILURE, payload: err.response })
+    );
+};
 
-export const USER_SIGNING = 'USER_SIGNING';
-export const USER_SIGNING_SUCCESS = 'USER_SIGNING_SUCCESS';
-export const USER_SIGNING_FAILURE = 'USER_SIGNING_FAILURE';
+export const USER_SIGNING = "USER_SIGNING";
+export const USER_SIGNING_SUCCESS = "USER_SIGNING_SUCCESS";
+export const USER_SIGNING_FAILURE = "USER_SIGNING_FAILURE";
 
 export const signUp = signUpData => dispatch => {
-    dispatch( { type:USER_SIGNING } )
+  dispatch({ type: USER_SIGNING });
 
-    axiosWithAuth()
+  axiosWithAuth()
     .post(`/api/auth/register`, signUpData)
-    .then( response => dispatch({ type: USER_SIGNING_SUCCESS, payload: response.data.user},
-        localStorage.setItem('token', response.data.token),
+    .then(response =>
+      dispatch(
+        { type: USER_SIGNING_SUCCESS, payload: response.data.user },
+        localStorage.setItem("token", response.data.token)
         // history.push('/login')
-    ))
-    .catch(err => dispatch({ type: USER_SIGNING_FAILURE, payload: err.response }))
-}
-export const START_FETCHING_DATA = 'START_FETCHING_DATA';
-export const FETCH_SUCCESS_DATA = 'FETCH_SUCCESS_DATA';
-export const FETCH_FAILURE_DATA = 'FETCH_FAILURE_DATA';
+      )
+    )
+    .catch(err =>
+      dispatch({ type: USER_SIGNING_FAILURE, payload: err.response })
+    );
+};
+export const START_FETCHING_DATA = "START_FETCHING_DATA";
+export const FETCH_SUCCESS_DATA = "FETCH_SUCCESS_DATA";
+export const FETCH_FAILURE_DATA = "FETCH_FAILURE_DATA";
 
 export const fetchingUserData = () => dispatch => {
-    dispatch({type: START_FETCHING_DATA})
+  dispatch({ type: START_FETCHING_DATA });
 
-    //-------- Get request here for Work out Card.
+  //-------- Get request here for Work out Card.
+  axiosWithAuth()
+    .get("https://weight-lift-journal-dev.herokuapp.com/api/users/profile")
+    .then(response => {
+      console.log(response.data);
+    })
 
-}
+    .catch(error => {
+      console.error("Server Error", error);
+    });
+};
 
+export const DELETING_DATA = "DELETING_DATA";
+export const DELETING_DATA_SUCCESS = "DELETING_DATA_SUCCESS";
+export const DELETING_DATA_FAILURE = "DELETING_DATA_FAILURE";
 
-
-export const DELETING_DATA = 'DELETING_DATA';
-export const DELETING_DATA_SUCCESS = 'DELETING_DATA_SUCCESS';
-export const DELETING_DATA_FAILURE = 'DELETING_DATA_FAILURE';
-
-export const POSTING_DATA = 'POSTING_DATA';
-export const POSTING_DATA_SUCCESS = 'POSTING_DATA_SUCCESS';
-export const POSTING_DATA_FAILURE = 'POSTING_DATA_FAILURE';
+export const POSTING_DATA = "POSTING_DATA";
+export const POSTING_DATA_SUCCESS = "POSTING_DATA_SUCCESS";
+export const POSTING_DATA_FAILURE = "POSTING_DATA_FAILURE";
